7fa003781fba3247acb55e4d1f2236658090d115
Harikrishna-Srinivas <harikrishna242426@gmail.com>
2021-12-13 15:30:30 +0530

    webserver

--- webserver.py
+++ webserver.py
@@ -1,25 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-
-To develop a simple webserver to serve html pages.
-## DESIGN STEPS:
-### Step 1:
-
-HTML content creation.
-### Step 2:
-
-Design of webserver workflow.
-### Step 3:
-
-Implementation using Python code.
-### Step 4:
-
-Serving the HTML pages.
-### Step 5:
-
-Testing the webserver.
-## PROGRAM:
-~~~
 from http.server import HTTPServer, BaseHTTPRequestHandler
 content = """
 <!DOCTYPE html>
@@ -28,7 +6,7 @@ content = """
 <title>Mywebserver</title>
 </head>
 <body>
-<h1>Hari krishna</h1>
+<h1>Harikrishna</h1>
 <h2>21002044</h2>
 <h4>harikrishna242426@gmail.com</h4>
 </body>
@@ -45,9 +23,3 @@ server_address = ('',8080)
 httpd = HTTPServer(server_address,myhandler)
 print("my webserver is running...")
 httpd.serve_forever()
-~~~
-
-## OUTPUT:
-![OUTPUT](/IMAGES/img50.png)
-## RESULT:
-Thus the program to create web repository is completed sucessfully.
